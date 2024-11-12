c5ab41338800e6cbd6a019df3beb965bcd792ddd
Gaurav Chopra <gmchopra@wisc.edu>
2024-11-12 15:40:13 -0600

    Main.py file

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from scraper import Scraper
-from embedder import Embedder
-from lecture_parser import Parser
-from db import Storage
+# from embedder import Embedder
+# from lecture_parser import Parser
+# from db import Storage
 import os
 
 scraper = Scraper()
 # srt_dir = scraper.execute()
-scraper.get_embed_link()
+scraper.get_embed_link("https://mediaspace.wisc.edu/media/Tyler%20Caraza-Harter-Agriculture%20125-09_04_24-14%3A18%3A34/1_sbftfkbl")
 
 # download_dir = os.getenv('SRT_PATH')
 # embedder = Embedder(download_dir)
@@ -16,7 +16,7 @@ scraper.get_embed_link()
 # c = p.parse_chunks("/Users/avitirto/Documents/ML/KalturaSearchSystem/lecture_srt/Tyler Caraza-Harter-Agriculture 125-09_04_24-14_18_34 (1).srt")
 # print(c)
 
-storage = Storage()
+# storage = Storage()
 # storage.db.delete_collection("Lectures")
 # print(storage.db.list_collections())
 # storage.add_lecture("/Users/avitirto/Documents/ML/KalturaSearchSystem/lecture_srt/Tyler Caraza-Harter-Agriculture 125-09_04_24-14_18_34 (1).srt")
