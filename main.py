88fe62f58a31d64f0e047a25181359cdadc3eedb
AviTirto <avi.tirto@gmail.com>
2024-11-11 18:47:11 -0600

    Streamlit frontend

--- main.py
+++ main.py
@@ -20,6 +20,6 @@ storage = Storage()
 # print(storage.db.list_collections())
 # storage.add_lecture("/Users/avitirto/Documents/ML/KalturaSearchSystem/lecture_srt/Tyler Caraza-Harter-Agriculture 125-09_04_24-14_18_34 (1).srt")
 
-r = storage.query("What is RAM?")
+r = storage.query("What is the main difference between a CPU and GPU?")
 print(r)
 print(len(r))
