49741b7fae8e716c60f4c313e972f461f313bc8e
Gaurav Chopra <gmchopra@wisc.edu>
2024-11-17 15:44:07 -0600

    pydantic model setup

--- server.py
+++ server.py
@@ -1,2 +1,12 @@
 import fastapi
+from query_manager import QueryManager
 
+app = fastapi()
+process_query = QueryManager()
+
+
+@app.get("/")
+def get_lecture_snippets(query : str):
+    x = process_query.query(query) 
+
+    
