a04ae2e2e9cf978d262033bba1d42cd9d3c2bf7b
AviTirto <avi.tirto@gmail.com>
2025-02-20 23:01:36 -0600

    implemented slide migration and populated ppts

--- backend/db/migrations/fill_slides_db.py
+++ backend/db/migrations/fill_slides_db.py
@@ -1,36 +1,91 @@
-# from app.services.PPT_manager import PPTManager
-# from app.models.ChromaModel.records import PPTcdb
-# import time
-
-# cdb = PPTcdb()
-# cdb.db.delete_collection('ppts')
-# ppt_manager = PPTManager()
-
-
-# # ppt_manager.pdf_files = ['./Econ_301_PPT/Chapter 12 PPT.pdf', './Econ_301_PPT/Chapter 2 PPT.pdf', './Econ_301_PPT/Chapter 5 PPT.pdf', './Econ_301_PPT/Chapter 14 PPT.pdf', './Econ_301_PPT/Chapter 15 PPT.pdf', './Econ_301_PPT/Chapter 6 PPT.pdf', './Econ_301_PPT/Chapter 7 PPT.pdf']
-
-
-# start = time.time()
-# ppt_manager.populate_ppts()
-# end = time.time()
-# print(end - start)
-
-# metas = ppt_manager.cdb.ppts.get()['metadatas']
-# docs = ppt_manager.cdb.ppts.get()['documents']
-# print(ppt_manager.cdb.db.get_collection('ppts').count())
-
 import os
-from backend.utils.cloudfareR2_tools.cloudfareR2_api import *
+import sys
+import asyncio
 
-econ_dir_path = r"./Econ_301_PPT"
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
+sys.path.insert(0, project_root)
+
+from backend.utils.cloudfareR2_tools.cloudfareR2_api import *
+from backend.utils.encoders import pdf_to_images, generate_unique_int64
+from backend.utils.gemini_tools.gemini_api import ocr_batch, get_ocr_llm
+from backend.utils.embedder import embed_text
+from backend.db.models import Slide, PPT
+from backend.utils.zilliz_tools.zilliz_api import get_conn, upload_slides
+from backend.utils.firebase_tools.firebase_api import get_db, add_slides_batch, add_ppts_batch
+
+econ_dir_path = os.path.join(project_root, "Econ_301_PPT")
+print(econ_dir_path)
+
+ocr_llm = get_ocr_llm()
+conn = get_conn()
+db = get_db()
 r2_client = get_cloudfareR2()
 
 if os.path.exists(econ_dir_path):
     for path in os.listdir(econ_dir_path):
+        print(path)
         file_path = os.path.join(econ_dir_path, path)
+        title = path.replace(" ", "-")
+        ppt_id = generate_unique_int64(title)
+
+        # Convert PDF to images
+        images_base64 = pdf_to_images(file_path)
+
+        # OCR Batch
+        print("Performing OCR...")
+        ocr_results = asyncio.run(ocr_batch(ocr_llm, images_base64))
+        print("OCR complete")
+
+        # Make slides
+        slides = []
+        for i in range(len(ocr_results)):
+            ocr_result = ocr_results[i]['result']
+            slides.append(
+                Slide(
+                    slide_id=generate_unique_int64(f"{title}-{2*i + 1}"),
+                    page_num = 2*i + 1,
+                    text=ocr_result.slide_1_text,
+                    ppt_id = ppt_id
+                )
+            )
+            if ocr_result.slide_2_text:
+                slides.append(
+                    Slide(
+                        slide_id=generate_unique_int64(f"{title}-{2*i + 2}"),
+                        page_num = 2*i + 2,
+                        text=ocr_result.slide_2_text,
+                        ppt_id = ppt_id
+                    )
+                )
+
+        # Zilliz Batch Insert
+        embeddings = []
+        slide_ids = []
+        
+        for slide in slides:
+            embeddings.append(embed_text(slide.text))
+            slide_ids.append(slide.slide_id)
+
+        print("Uploading slides to Zilliz...")
+        upload_slides(conn, slide_ids, embeddings)
+        print("Upload complete")
+        print("Zilliz insertion complete")
+
+        # Firestore Batch Insert
+        print("Inserting slides into Firestore...")
+        add_slides_batch(db, slides)
+        print("Firestore insertion complete")
+        print("Inserting ppt into Firestore...")
+        add_ppts_batch(db, [PPT(ppt_id=ppt_id, title=title, path = "Econ-301/" + title)])
+        print("Firestore insertion complete")
+
+        # R2 Batch Insert
+        print("Uploading slides to R2...")
         with open(file_path, 'rb') as file:
             data = file.read()
             object_key = "Econ-301/" + path.replace(" ", "-") # Making format compatible with what R2 wants
             upload_file(r2_client, data, object_key)
+        print("R2 upload complete")
+        print(f"Successfully inserted {title}")
 
 
--- backend/utils/encoders.py
+++ backend/utils/encoders.py
@@ -1,4 +1,7 @@
 import hashlib
+from pdf2image import convert_from_path
+import io
+import base64
 
 def generate_unique_int64(input_string):
     sha256_hash = hashlib.sha256(input_string.encode('utf-8')).hexdigest()
@@ -9,3 +12,14 @@ def generate_unique_int64(input_string):
     int64_value = int64_value % MAX_INT64
 
     return int64_value
+
+def pdf_to_images(pdf_path):
+    images = convert_from_path(pdf_path)
+    images_base64 = []
+    for image in images:
+        img_bytes = io.BytesIO()
+        image.save(img_bytes, format="JPEG")  # Save the image as JPEG
+        img_bytes = img_bytes.getvalue()
+        images_base64.append(base64.b64encode(img_bytes).decode("utf-8"))
+    return images_base64
+
